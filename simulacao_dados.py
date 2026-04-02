c869f3074f62d764cd6f280a056ddc38a0c5600c
DiegoHandlovicsTavares <diegohandlovicst@gmail.com>
2026-04-02 14:55:32 -0300

    Update simulacao_dados.py

--- simulacao_dados.py
+++ simulacao_dados.py
@@ -1,8 +1,7 @@
 import random
 
-d1 = random.randint(1, 6)
-d2 = random.randint(1, 6)
-
-resultado = d1 + d2
-
-print("Resultado da rolagem:", resultado)
+for _ in range(10):
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    resultado = d1 + d2
+    print("Resultado:", resultado)
